de9d27ff202c3e519f6e8e142c9d3d21816be057
Nguyễn Việt Chung <chung24mse13163@fsb.edu.vn>
2024-09-16 13:56:49 +0700

    Dev

--- libs/rabbitmq.py
+++ libs/rabbitmq.py
@@ -1,5 +1,39 @@
 import pika
+import json
 
-connection = pika.BlockingConnection(pika.ConnectionParameters("localhost"))
-recognize_channel = connection.channel()
-recognize_channel.queue_declare(queue="recognize")
+
+class RabbitMQ:
+    def __init__(self, host="localhost", queue_name="recognize"):
+        self.host = host
+        self.queue_name = queue_name
+        self.connection = None
+        self.channel = None
+
+    def connect(self):
+        self.connection = pika.BlockingConnection(pika.ConnectionParameters(self.host))
+        self.channel = self.connection.channel()
+        self.channel.queue_declare(queue=self.queue_name)
+
+    def close(self):
+        if self.connection:
+            self.connection.close()
+
+    def publish(self, message):
+        if not self.channel:
+            self.connect()
+        self.channel.basic_publish(
+            exchange="", routing_key=self.queue_name, body=json.dumps(message)
+        )
+
+    def consume(self, callback):
+        if not self.channel:
+            self.connect()
+        self.channel.basic_qos(prefetch_count=1)
+        self.channel.basic_consume(
+            queue=self.queue_name, on_message_callback=callback, auto_ack=False
+        )
+        print(" [*] Waiting for messages. To exit press CTRL+C")
+        self.channel.start_consuming()
+
+    def acknowledge(self, delivery_tag):
+        self.channel.basic_ack(delivery_tag=delivery_tag)
